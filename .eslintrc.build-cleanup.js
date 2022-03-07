0091f60af95818c9f623dc3ed2f7e17a5f7c7a20
Christian Vetter <hallo@cv-o.de>
2022-03-07 15:50:38 +0100

    fix: fix: support down to node v12

--- .eslintrc.build-cleanup.js
+++ .eslintrc.build-cleanup.js
@@ -3,6 +3,10 @@ const DEACTIVATE = `off`
 module.exports = {
 	// non-fixable rules that don't help when applied after transpilation and bundling
 	rules: {
+		"no-cond-assign": DEACTIVATE,
+		"no-return-assign": DEACTIVATE,
+
+		"@typescript-eslint/init-declarations": DEACTIVATE,
 		"@typescript-eslint/no-magic-numbers": DEACTIVATE,
 		"@typescript-eslint/no-shadow": DEACTIVATE,
 		"@typescript-eslint/no-unsafe-argument": DEACTIVATE,
@@ -10,6 +14,7 @@ module.exports = {
 		"@typescript-eslint/no-unsafe-call": DEACTIVATE,
 		"@typescript-eslint/no-unsafe-member-access": DEACTIVATE,
 		"@typescript-eslint/no-unsafe-return": DEACTIVATE,
+		"@typescript-eslint/no-unused-expressions": DEACTIVATE,
 		"@typescript-eslint/no-var-requires": DEACTIVATE,
 		"@typescript-eslint/strict-boolean-expressions": DEACTIVATE,
 	},
